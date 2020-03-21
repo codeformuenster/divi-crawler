b57c31c30c313b9af867e2ad31a2f9ea5eae8963
Thorben Jensen <jensen.thorben@gmail.com>
2020-03-21 21:41:15 +0100

    add python installation setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+from glob import glob
+from setuptools import setup, find_packages
+
+setup(
+    name="divi-crawler",
+    version="0.1.0",
+    author="CodeForMuenster",
+    url="https://github.com/codeformuenster/divi-crawler",
+    license="MIT",
+    # source
+    packages=find_packages(),
+    scripts=glob("scripts/*"),
+    # dependencies
+    install_requires=["bs4", "requests", "pytz"],
+    extras_require={
+        "dev": ["black", "pylama", "rope"],
+    },
+)
