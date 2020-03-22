6255ef72e07c139d05f0ca958d873ad84fb6eae9
Thorben Jensen <jensen.thorben@gmail.com>
2020-03-22 11:13:16 +0100

    black autoformat

--- scripts/mongoDBUpload.py
+++ scripts/mongoDBUpload.py
@@ -3,15 +3,21 @@ import pymongo
 USER_NAME = "root"
 PASSWORD = "challenge1757"
 
+
 def _getConnection():
-    return pymongo.MongoClient("mongodb://" + USER_NAME + ":" + PASSWORD + "@bene.gridpiloten.de:27017/")
-  
+    return pymongo.MongoClient(
+        "mongodb://" + USER_NAME + ":" + PASSWORD + "@bene.gridpiloten.de:27017/"
+    )
+
+
 def _getRawDB():
     return _getConnection()["raw"]
 
+
 def _insertMeta(meta):
     return _getRawDB()["medical_resorce_meta"].insert(meta)
-    
+
+
 def insertData(dataList, meta):
     meta = _insertMeta(meta)
     for data in dataList:
