6d532acab0118d4af8bc703788d691b1f0ba4514
Thorben Jensen <jensen.thorben@gmail.com>
2020-03-31 23:02:59 +0200

    update library version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="divi-crawler",
-    version="0.1.0",
+    version="0.1.1",
     author="CodeForMuenster",
     url="https://github.com/codeformuenster/divi-crawler",
     license="MIT",
